b27053fdd8dfd87fe77f51e690e0d3e391a3672a
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-22 21:50:16 -0700

    Adds pseudocode notes

--- sorting/BubbleSort.js
+++ sorting/BubbleSort.js
@@ -12,3 +12,10 @@ const bubbleSort = (array) => {
   }
   return array;
 }
+// IN: unsorted array of n size
+// OUT: sorted array of n size
+
+// FOR the length of the array 
+// IF array[n] is greater than array[n+1]
+// SWAP array[n] and array[n+1]
+// RETURN array
