be0890c5307bafe5ec1ce4f9431c6c2101fd80fa
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-03-25 00:58:22 -0700

    Convert to ES6

--- sorting/MergeSort.js
+++ sorting/MergeSort.js
@@ -1,18 +1,16 @@
-function mergeSort(arr)
-{
+const mergeSort = (arr) => {
     if (arr.length < 2)
         return arr;
  
-    var middle = parseInt(arr.length / 2);
-    var left   = arr.slice(0, middle);
-    var right  = arr.slice(middle, arr.length);
+    const middle = parseInt(arr.length / 2);
+    const left   = arr.slice(0, middle);
+    const right  = arr.slice(middle, arr.length);
  
     return merge(mergeSort(left), mergeSort(right));
-}
+};
  
-function merge(left, right)
-{
-    var result = [];
+const merge = (left, right) => {
+    const result = [];
  
     while (left.length && right.length) {
         if (left[0] <= right[0]) {
@@ -29,4 +27,6 @@ function merge(left, right)
         result.push(right.shift());
  
     return result;
-}
+};
+
+console.log(mergeSort([3,5,6,7,2,4,1,7,4,2]))
